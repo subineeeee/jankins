8298cb3e1dd6de9956e5f75f1815d5f30b7c2da9
subin <tnqls1359@naver.com>
2021-12-21 16:50:27 +0900

    edit

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(){
         return "Hello";
     }
